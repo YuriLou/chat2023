5a6d22c3ff318443cf22bf5f1e39eebc467b66fa
YuriLou <yuri.lou.ri@gmail.com>
2023-08-30 17:13:29 -0300

    ajeitei

--- chat.php
+++ chat.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat Aralho</title>
-    <link rel="stylesheet" href="stylochat.css">
-</head>
-<body>
-    <div class="container">
+<div class="container">
         <label for="chat">Escreva aqui a sua mensagem: </label>
             <input class="chat" type="text" placeholder="Escreva alguma coisa aqui aralho" >
     </div>
-</body>
-</html>
