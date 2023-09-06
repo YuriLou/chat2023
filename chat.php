58f047dd31f6399f1d9c71eebdcb2380f7c93dcd
NaelTakamoto <viniciusnael@gmail.com>
2023-09-06 14:26:20 -0300

    retirei o nome de usuario

--- chat.php
+++ chat.php
@@ -7,10 +7,6 @@
         <link rel="stylesheet" href="stylochat.css">
 </head>
 <body>
-       <div class="container">
-                <label for="user">Nome de usuário:</label>
-                <textarea name="username" id="username" cols="30" rows="10" placeholder="Escreva aqui o nome de usuário"></textarea>
-       </div> 
 
        <div class="container">
                 <label for="mensagem">Digite a mensagem: </label>
