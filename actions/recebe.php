6fd7c1f289a6b5a386a4bce1c1e67b0d72258f4b
YuriLou <yuri.lou.ri@gmail.com>
2023-09-06 16:31:13 -0300

    php

--- actions/recebe.php
+++ actions/recebe.php
@@ -4,9 +4,9 @@
 require '../includes/conexao.php';
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_EMAIL);
+$data = date("Y-m-d H:i:s");
 
-
-$sql = "INSERT INTO mensagens (nome, mensagem) VALUES ('$nome','$mensagem')";
+$sql = "INSERT INTO mensagens (nome, mensagem, datahora) VALUES ('$nome','$mensagem', '$data')";
 
 $conexao->query($sql);
 
