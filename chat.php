5128dee317b0ddba23cf511a5523c4cf462996f7
dani <eu@gmail.com>
2023-09-06 15:03:22 -0300

    asfsdf

--- chat.php
+++ chat.php
@@ -10,15 +10,18 @@
 
 <body>
 
-        <img src="images/dog rebaixado.webp" alt="">
-       <div class="container">
+        <div class="chat">
+         <img src="images/dog rebaixado.webp" alt="">
+         <div class="container">
                 <label for="mensagem">Digite a mensagem: </label>
                 <textarea name="mensagem" id="mensagem" cols="30" rows="10" placeholder="Digite aqui a mensagem a ser enviada"></textarea>
-       </div>
+         </div>
 
-       <div class="container">
+         <div class="container">
                 <button>ENVIAR!</button>
-       </div>
+         </div>
+        </div>
+
 </body>
 
 </html>
