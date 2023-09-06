5752dfba0187b1baad690b6b298db41d8323c4e4
João Paulo <jpmadeinzimba@gmail.com>
2023-09-06 14:21:55 -0300

    fiz grandes mudanças no index e criei uma estilização para  mesmo

--- index.php
+++ index.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat GPB</title>
-    <link rel="stylesheet" href="stylochat.css">
+    <link rel="stylesheet" href="styloindex.css">
 </head>
 <body>
-    <header></header>
-    <main>
+    <div class="login-container">
+        <h2>Login</h2>
+        <form action="chat.php" method="POST">
+            <label for="username">Nome de usuário:</label>
+            <input type="text" id="username" name="username" placeholder="Digite seu nome de usuário" required>
 
 
-    </main>
-    <footer>Técnico Integrado em Informática - 21
-    </footer>
+            <button type="submit">Entrar</button>
+        </form>
+    </div>
 </body>
 </html>
