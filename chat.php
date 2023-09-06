74bda104d6135934fa7894bbd66dd1208424470e
YuriLou <yuri.lou.ri@gmail.com>
2023-09-06 14:35:48 -0300

    forms

--- chat.php
+++ chat.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Chat</title>
         <link rel="stylesheet" href="stylochat.css">
 </head>
+
 <body>
 
         <img src="images/dog rebaixado.webp" alt="">
-       <div class="container">
-                <label for="mensagem">Digite a mensagem: </label>
-                <textarea name="mensagem" id="mensagem" cols="30" rows="10" placeholder="Digite aqui a mensagem a ser enviada"></textarea>
-       </div>
-       <input type="hidden" value="a" name="teste">
-       <div class="container">
-                <button>ENVIAR!</button>
-       </div>
+        <form action="actions/recebe.php">
+                <div class="container">
+                        <label for="mensagem">Digite a mensagem: </label>
+                        <textarea name="mensagem" id="mensagem" cols="30" rows="10" placeholder="Digite aqui a mensagem a ser enviada"></textarea>
+                </div>
+
+                <div class="container">
+                        <button>ENVIAR!</button>
+                </div>
+        </form>
+
 </body>
+
 </html>
