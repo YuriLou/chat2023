9d4154dcc87f1533ef14812a7c216cfa2805115a
dani <eu@gmail.com>
2023-09-13 14:16:50 -0300

    ler.php

---
+++ actions/ler.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbname = 'bd_0830';
+$user = 'root';
+$password = '';
+
+$conexao = new PDO('mysql:host=localhost; dbname='.$dbname,$user,$password);
+
+$stmt = $conexao->query("select * from mensagens order by id desc");
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($data);
